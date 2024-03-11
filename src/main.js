8f273487e3145492209f682aa60dcff1a34bdaa1
rooyca <rooyca@gmail.com>
2024-03-11 12:59:14 -0500

    update: first version

--- src/main.js
+++ src/main.js
@@ -1,8 +1,31 @@
+const title = document.querySelector('.heading__primary');
+const director = document.querySelector('.heading__secondary');
+const description = document.querySelector('.movie__description');
+const year = document.querySelector('.year');
+const tag1 = document.querySelector('.tag1');
+const tag2 = document.querySelector('.tag2');
+const length = document.querySelector('.length');
+const date = document.querySelector('.movie__date');
+
 const GetInfo = () => {
-	fetch('https://raw.githubusercontent.com/Rooyca/1001M/master/current.json')
+	fetch('current.json')
 		.then(response => response.json())
 		.then(data => {
 			console.log(data);
+			title.innerHTML = data.title;
+			director.innerHTML = data.director;
+			description.innerHTML = data.desc;
+			year.innerHTML = "📅 "+data.year;
+			length.innerHTML = "🕒 "+data.length;
+
+			for (let i = 0; i < data.tags.length; i++) {
+				if (i === 0) {
+					tag1.innerHTML = "🏷️ #"+data.tags[i];
+				} else {
+					tag2.innerHTML = "🏷️ #"+data.tags[i];
+				}
+			}
+			date.innerHTML = new Date().toDateString();
 		})
 		.catch(error => {
 			console.error('Error:', error);
