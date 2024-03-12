7f10eb4df76a079aeda8d078730e83704d1e104a
Rooyca <42101257+Rooyca@users.noreply.github.com>
2024-03-12 04:32:14 +0000

    Deploying to gh-pages from @ Rooyca/1001M@2d9cc90f98e343fe9af7638b5ac4eb73490591f2 🚀

---
+++ main.js
@@ -0,0 +1,35 @@
+const title = document.querySelector('.heading__primary');
+const director = document.querySelector('.heading__secondary');
+const description = document.querySelector('.movie__description');
+const year = document.querySelector('.year');
+const tag1 = document.querySelector('.tag1');
+const tag2 = document.querySelector('.tag2');
+const length = document.querySelector('.length');
+const date = document.querySelector('.movie__date');
+const poster = document.querySelector('.movie__img');
+
+const GetInfo = () => {
+	fetch('current.json')
+		.then(response => response.json())
+		.then(data => {
+			poster.src = data.img;
+			title.innerHTML = data.title;
+			director.innerHTML = data.director;
+			description.innerHTML = data.desc;
+			year.innerHTML = "📅 "+data.year;
+			length.innerHTML = "🕒 "+data.length;
+
+			for (let i = 0; i < data.tags.length; i++) {
+				if (i === 0) {
+					tag1.innerHTML = "🏷️ #"+data.tags[i];
+				} else {
+					tag2.innerHTML = "🏷️ #"+data.tags[i];
+				}
+			}
+			date.innerHTML = new Date().toDateString();
+		})
+		.catch(error => {
+			console.error('Error:', error);
+		});
+}
+GetInfo();
